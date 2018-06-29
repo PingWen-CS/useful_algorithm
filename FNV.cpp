5039ee08a4418e727ac40a14d0485fda1a75937f
PingWen-CS <494301629@qq.com>
2018-06-29 15:59:30 +0800

    Update FNV.cpp

--- FNV.cpp
+++ FNV.cpp
@@ -1,6 +1,6 @@
 #32 bit FNV hash
 uint32_t hash(const std::string& str) {
-  uint32_t h = 2166136261
+  uint32_t h = 2166136261;
   for (size_t i=0; i< str.size(); i++) {
     h = h ^ unit32_t(str(i));
     h = h * 16777619;
